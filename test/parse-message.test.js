99c694ae59b2265415a90559be01c1a546ff8e3b
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-10 00:28:12 -0700

    message parser parses

--- test/parse-message.test.js
+++ test/parse-message.test.js
@@ -2,10 +2,13 @@ const assert = require('assert');
 const parseMessage = require('../lib/parse-message');
 
 describe('message parser', () => {
-    const message = '@cmd:param some text';
-
     it('ignores strings that do not start with @', () => {
         const failure = parseMessage('some text without @ at the beginning');
         assert.strictEqual(failure, null);
+    });
+
+    it('converts a string into a command object', () => {
+        const messageObject = parseMessage('@cmd:param some text');
+        assert.deepStrictEqual(messageObject, { command: 'cmd', argument: 'param', text: 'some text' });
     });
 });
