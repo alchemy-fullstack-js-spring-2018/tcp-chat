29b77ceea34335ba1bea59919a1c5aaf76416bb4
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-09 23:30:30 -0700

    failing renaming test

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -4,18 +4,29 @@ const ChatRoom = require('../lib/chat-room');
 describe('chat room', () => {
     const newClient = {};
 
-    let chatRoom, clients, username;
+    let chatRoom, clients, originalName;
     beforeEach(() => {
         chatRoom = new ChatRoom();
         clients = chatRoom.clients;
-        username = chatRoom.add(newClient);
+        originalName = chatRoom.add(newClient);
     });
 
     it('takes a socket, assigns a username, and stores by username', () => {
-        const added = clients.has(username);
-        const clientObject = chatRoom.getClient(username);
+        const added = clients.has(originalName);
+        const clientObject = chatRoom.getClient(originalName);
         assert.strictEqual(added, true);
-        assert.strictEqual(clientObject.username, username);
+        assert.strictEqual(clientObject.username, originalName);
         assert.strictEqual(clientObject, newClient);
+    });
+
+    it('renames a user', () => {
+        const newName = 'new name';
+        const renamed = chatRoom.rename(originalName, newName);
+        const searchInVain = chatRoom.getClient(originalName);
+        const clientObject = chatRoom.getClient(newName);
+        assert.strictEqual(renamed, true);
+        assert.strictEqual(searchInVain, undefined);
+        assert.strictEqual(clientObject, newClient);
+        assert.strictEqual(clientObject.username, newName);        
     });
 });
