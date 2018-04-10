db698ce316fff9f5e5e2ec91e251c887490d4abc
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-09 23:18:39 -0700

    refactor test

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -2,16 +2,16 @@ const assert = require('assert');
 const ChatRoom = require('../lib/chat-room');
 
 describe('chat room', () => {
+    const newClient = {};
 
-    let chatRoom;
+    let chatRoom, clients, username;
     beforeEach(() => {
         chatRoom = new ChatRoom();
+        clients = chatRoom.clients;
+        username = chatRoom.add(newClient);
     });
 
     it('takes a socket, assigns a username, and stores by username', () => {
-        const clients = chatRoom.clients;
-        const newClient = {};
-        const username = chatRoom.add(newClient);
         const added = clients.has(username);
         const clientObject = chatRoom.getClient(username);
         assert.strictEqual(added, true);
